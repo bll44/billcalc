91c386e8d1e8d8c65e04614246fdaea220be97b4
vagrant <vagrant@homestead>
2016-02-03 23:51:16 +0000

    quick update to the database migration files

--- database/migrations/2015_12_30_173717_bills_tbl.php
+++ database/migrations/2015_12_30_173717_bills_tbl.php
@@ -14,14 +14,6 @@ class BillsTbl extends Migration
     {
         Schema::create('bills', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('residence_id');
-            $table->integer('resident_id');
-            $table->string('month_of');
-            $table->integer('year_of');
-            $table->decimal('cable_amt', 10, 2);
-            $table->decimal('gas_amt', 10, 2);
-            $table->decimal('water_amt', 10, 2);
-            $table->decimal('electric_amt', 10, 2);
             $table->timestamps();
         });
     }
